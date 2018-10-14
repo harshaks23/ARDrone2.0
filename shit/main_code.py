d207dc700b526e77545d0f4603cebff3a511b247
harshaks23 <harshaksabbur@gmail.com>
2018-10-14 11:35:44 -0500

    das

---
+++ shit/main_code.py
@@ -0,0 +1,146 @@
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+#import argparse
+import imutils
+import time
+import cv2,numpy,os
+#import parse_sensor_data 
+import ardrone
+import argparse
+
+from PIL import Image
+
+#from twilio.rest import Client
+
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.2,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+# load our serialized model from disk
+net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+# Load the trained mode
+recognizer.read('trainer/trainer.yml')
+
+# Load prebuilt model for Frontal Face
+cascadePath = "face.xml"
+
+# Create classifier from prebuilt model
+faceCascade = cv2.CascadeClassifier(cascadePath);
+from twilio.rest import Client
+# Set the font style
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Initialize and start the video frame capture
+cam = cv2.VideoCapture(0)
+
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    
+    #frame=vs.read()
+	ret, im =cam.read()
+	frame=im
+    # Convert the captured frame into grayscale
+        gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+
+    # Get all face from the video frame
+        faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
+        frame = imutils.resize(frame, width=650)
+
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (650, 650)),
+		0.007843, (650, 650), 127.5)
+
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	net.setInput(blob)
+	detections = net.forward()
+
+	# loop over the detections
+	for i in np.arange(0, detections.shape[2]):
+		# extract the confidence (i.e., probability) associated with
+		# the prediction
+		confidence = detections[0, 0, i, 2]
+
+		# filter out weak detections by ensuring the `confidence` is
+		# greater than the minimum confidence
+		if confidence > args["confidence"]:
+			# extract the index of the class label from the
+			# `detections`, then compute the (x, y)-coordinates of
+			# the bounding box for the object
+			idx = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			# draw the prediction on the frame
+			label = "{}: {:.2f}%".format(CLASSES[idx],
+				confidence * 100)
+			cv2.rectangle(frame, (startX, startY), (endX, endY),
+				COLORS[idx], 2)
+			y = startY - 15 if startY - 15 > 15 else startY + 15
+			cv2.putText(frame, label, (startX, y),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+	# show the output frame
+	cv2.imshow("Object Detection", frame)
+
+    # For each face in faces
+        for(x,y,w,h) in faces:
+		cv2.rectangle(im, (x-20,y-20), (x+w+20,y+h+20), (0,255,0), 4)
+
+        # Recognize the face belongs to which ID
+		Id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        #print(Id)
+        # Check the ID if exist 
+        	if(Id == 1):
+			Id = "harsha, Male Chicago {0:.2f}%".format(round(100 - confidence, 2))
+        	elif(Id == 2):
+            		Id = "Jonathan, Male Champaign{0:.2f}%".format(round(100 - confidence, 2))
+
+        # Put text describe who is in the picture
+        	cv2.rectangle(im, (x-22,y-90), (x+w+22, y-22), (0,255,0), -1)
+        	cv2.putText(im, str(Id), (x,y-40), font, 1, (255,255,255), 3)
+
+    # Display the video frame with the bounded rectangle
+    	cv2.imshow('Face Recognition',im)  
+
+    # If 'q' is pressed, close program
+    	if cv2.waitKey(10) & 0xFF == ord('q'):
+        	break
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
